77f0738464a7064938538e709d6a5d1b9db3e30f
Nik Moraitis <47322413+nikmors@users.noreply.github.com>
2019-03-25 10:44:14 +0000

    Add files via upload

---
+++ Recipes-CSS3 polygons circular images/js_1.js
@@ -0,0 +1,102 @@
+// JavaScript Document
+$(document).ready(function() {
+ 
+        //Calculate the height of <header>
+        //Use outerHeight() instead of height() if have padding
+        var aboveHeight = $('header').outerHeight();
+ 
+	//when scroll
+        $(window).scroll(function(){
+ 
+	        //if scrolled down more than the header’s height
+                if ($(window).scrollTop() > aboveHeight){
+ 
+	        // if yes, add “fixed” class to the <nav>
+	        // add padding top to the #content 
+                //(value is same as the height of the nav)
+                $('nav').addClass('fixed').css('top','0').next()
+                .css('padding-top','60px');
+ 
+                } else {
+ 
+	        // when scroll up or less than aboveHeight,
+                //remove the “fixed” class, and the padding-top
+                $('nav').removeClass('fixed').next()
+                .css('padding-top','0');
+                }
+        });
+});
+
+/*--------start rating stars-----------*/
+  $(document).ready(function() {
+        
+        $('.rate_widget').each(function(i) {
+            var widget = this;
+            var out_data = {
+                widget_id : $(widget).attr('id'),
+                fetch: 1
+            };
+            $.post(
+                'ratings.php',
+                out_data,
+                function(INFO) {
+                    $(widget).data( 'fsr', INFO );
+                    set_votes(widget);
+                },
+                'json'
+            );
+        });
+    
+
+        $('.ratings_stars').hover(
+            // Handles the mouseover
+            function() {
+                $(this).prevAll().andSelf().addClass('ratings_over');
+                $(this).nextAll().removeClass('ratings_vote'); 
+            },
+            // Handles the mouseout
+            function() {
+                $(this).prevAll().andSelf().removeClass('ratings_over');
+                // can't use 'this' because it wont contain the updated data
+                set_votes($(this).parent());
+            }
+        );
+        
+        
+        // This actually records the vote
+        $('.ratings_stars').bind('click', function() {
+            var star = this;
+            var widget = $(this).parent();
+            
+            var clicked_data = {
+                clicked_on : $(star).attr('class'),
+                widget_id : $(star).parent().attr('id')
+            };
+            $.post(
+                'ratings.php',
+                clicked_data,
+                function(INFO) {
+                    widget.data( 'fsr', INFO );
+                    set_votes(widget);
+                },
+                'json'
+            ); 
+        });
+        
+        
+        
+    });
+
+    function set_votes(widget) {
+
+        var avg = $(widget).data('fsr').whole_avg;
+        var votes = $(widget).data('fsr').number_votes;
+        var exact = $(widget).data('fsr').dec_avg;
+    
+        window.console && console.log('and now in set_votes, it thinks the fsr is ' + $(widget).data('fsr').number_votes);
+        
+        $(widget).find('.star_' + avg).prevAll().andSelf().addClass('ratings_vote');
+        $(widget).find('.star_' + avg).nextAll().removeClass('ratings_vote'); 
+        $(widget).find('.total_votes').text( votes + ' votes recorded (' + exact + ' rating)' );
+    }
+/*--------end rating stars-----------*/
